3513c544425a49c83ebd6ffe9b6765fbe849f409
thinhoigami <nguyen.tien.thinh.thinhoigami@gmail.com>
2023-03-21 22:14:15 +0700

    initial

---
+++ duan1/src/main/java/com/nhom2/duan1/Duan1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nhom2.duan1;
+
+/**
+ *
+ * @author thinhorigami
+ */
+public class Duan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
