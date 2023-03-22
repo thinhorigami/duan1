cef0d79ec2acc4d198fbbfa365269abdc70b5a8a
DESKTOP-KU4PIO2\Phuong Bi <nguyenphuongsusi1312@gmail.com>
2023-03-22 22:09:02 +0700

    tạo package

---
+++ duan1/src/main/java/Domainmodel/KhachHang.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domainmodel;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Phuong Bi
+ */
+public class KhachHang {
+     private String id;
+    private String ma;
+    private String ten;
+    private String gioiTinh;
+    private String diaChi;
+    private Date ngaySinh;
+    private String dienThoai;
+    private String trangThai;
+    
+  
+
+    public KhachHang() {
+    }
+
+    public KhachHang(String id, String ma, String ten, String gioiTinh, String diaChi, Date ngaySinh, String dienThoai, String trangThai) {
+        this.id = id;
+        this.ma = ma;
+        this.ten = ten;
+        this.gioiTinh = gioiTinh;
+        this.diaChi = diaChi;
+        this.ngaySinh = ngaySinh;
+        this.dienThoai = dienThoai;
+        this.trangThai = trangThai;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getDienThoai() {
+        return dienThoai;
+    }
+
+    public void setDienThoai(String dienThoai) {
+        this.dienThoai = dienThoai;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+  
+}
---
+++ duan1/src/main/java/Repositories/KhachHangRepository.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Repositories;
+
+import Domainmodel.KhachHang;
+import Utilities.DBContext;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Phuong Bi
+ */
+public class KhachHangRepository {
+
+    public ArrayList<KhachHang> getAll() throws SQLException {
+        ArrayList<KhachHang> n = new ArrayList<>();
+        Connection conn = DBContext.getConnection();
+        String sql = "select * from khachhang";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            n.add(new KhachHang(rs.getString("id"), rs.getString("ma"),
+                    rs.getString("ten"), rs.getString("gioi_tinh"),
+                    rs.getString("dia_chi"), rs.getDate("ngay_sinh"),
+                    rs.getString("so_dien_htoai"), rs.getString("trang_thai")));
+
+        }
+        return n;
+
+    }
+
+    public boolean add(KhachHang n) throws SQLException {
+        try {
+            Connection conn = DBContext.getConnection();
+            String sql = "INSERT INTO KhachHang\n"
+                    + "                  (ma, ten, gioi_tinh, dia_chi, ngay_sinh, so_dien_htoai, trang_thai)\n"
+                    + "VALUES (?,?,?,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, n.getMa());
+            ps.setString(2, n.getTen());
+            ps.setString(3, n.getGioiTinh());
+            ps.setString(4, n.getDiaChi());
+            ps.setDate(5, (Date) n.getNgaySinh());
+            ps.setString(6, n.getDienThoai());
+            ps.setString(7, n.getTrangThai());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException sQLException) {
+            return false;
+        }
+
+    }
+
+    public boolean update(String ma, KhachHang n) throws SQLException {
+        try {
+            Connection conn = DBContext.getConnection();
+            String sql = "UPDATE KhachHang\n"
+                    + "SET          ten =?, gioi_tinh =?, dia_chi =?, ngay_sinh =?, so_dien_htoai =?, trang_thai =? where ma=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+//            ps.setString(1, n.getMa());
+            ps.setString(1, n.getTen());
+            ps.setString(2, n.getGioiTinh());
+            ps.setString(3, n.getDiaChi());
+            ps.setDate(4, (Date) n.getNgaySinh());
+            ps.setString(5, n.getDienThoai());
+            ps.setString(6, n.getTrangThai());
+            ps.setString(7, ma);
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException sQLException) {
+            return false;
+        }
+    }
+
+    public boolean delete(String ma) throws SQLException {
+        try {
+            Connection conn = DBContext.getConnection();
+            String sql = "delete khachhang where ma=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, ma);
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException sQLException) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        KhachHangRepository k = new KhachHangRepository();
+        k.delete("KH1");
+    }
+}
