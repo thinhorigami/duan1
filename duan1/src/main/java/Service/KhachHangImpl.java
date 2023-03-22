99f09fb4b3472e32744612a7c05ea60e52b4559c
DESKTOP-KU4PIO2\Phuong Bi <nguyenphuongsusi1312@gmail.com>
2023-03-22 23:04:31 +0700

    update khách hàng

--- duan1/src/main/java/Service/KhachHangImpl.java
+++ duan1/src/main/java/Service/KhachHangImpl.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package ServiceImpl;
+package Service;
 
+import ServiceImpl.*;
 import Domainmodel.KhachHang;
 import java.util.ArrayList;
 
--- duan1/src/main/java/ServiceImpl/KhachHangService.java
+++ duan1/src/main/java/ServiceImpl/KhachHangService.java
@@ -2,11 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Service;
+package ServiceImpl;
+
 
 import Domainmodel.KhachHang;
 import Repositories.KhachHangRepository;
-import ServiceImpl.KhachHangImpl;
+import Service.KhachHangImpl;
+
 import java.util.ArrayList;
 
 /**
