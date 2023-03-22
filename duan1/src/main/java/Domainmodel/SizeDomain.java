033c07f39fb67a834746a9f942097438f5a3c2da
vinh2904vinh <phamhongvvinh1@gmail.com>
2023-03-22 22:48:34 +0700

    sp

---
+++ duan1/src/main/java/Domainmodel/SizeDomain.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domainmodel;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class SizeDomain {
+
+    private String id;
+    private String ma;
+    private String ten;
+    private String trangThai;
+
+    public SizeDomain() {
+    }
+
+    public SizeDomain(String ma, String ten, String trangThai) {
+        this.ma = ma;
+        this.ten = ten;
+        this.trangThai = trangThai;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    
+}
