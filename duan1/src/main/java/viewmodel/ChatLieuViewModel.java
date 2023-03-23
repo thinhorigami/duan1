89135f6773b6077447fec12e66c9fbf3a47fbbc3
vinh2904vinh <phamhongvvinh1@gmail.com>
2023-03-23 16:01:20 +0700

    viewModel

---
+++ duan1/src/main/java/viewmodel/ChatLieuViewModel.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package viewmodel;
+
+/**
+ *
+ * @author admin
+ */
+public class ChatLieuViewModel {
+
+    private String id;
+    private String ma;
+    private String ten;
+    private String trangThai;
+
+    public ChatLieuViewModel() {
+    }
+
+    public ChatLieuViewModel(String id, String ma, String ten, String trangThai) {
+        this.id = id;
+        this.ma = ma;
+        this.ten = ten;
+        this.trangThai = trangThai;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    
+    
+     public Object[] toDataRow(){
+        return new Object[]{id,ma,ten,trangThai};
+    }
+}
