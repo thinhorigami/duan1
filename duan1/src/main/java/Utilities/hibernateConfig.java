9b2967eabf05e7934a6e0612b673352d880bb4ed
tuphph24187 <concuakicuc2k@gmail.com>
2023-03-25 00:51:58 +0700

    no message

--- duan1/src/main/java/Utilities/hibernateConfig.java
+++ duan1/src/main/java/Utilities/hibernateConfig.java
@@ -1,69 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Utilities;
 
-
-
-import com.microsoft.sqlserver.jdbc.SQLServerDriver;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import Domainmodel.KhuyenMai;
-import java.util.Properties;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-/**
- *
- * @author concu
- */
+import java.util.Properties;
+
 public class hibernateConfig {
-
-    private static final String USERNAME = "sa";
-    private static final String PASSWORD = "123";
-    private static final String SERVER_NAME = "localhost";
-    private static final String PORT = "1433";
-    private static final String DATABASE_NAME = "Nhom2_DuAn1";
-    private static final boolean USING_SSL = true;
-
-    private static String CONNECT_STRING;
-
-    static {
-        try {
-            DriverManager.registerDriver(new SQLServerDriver());
-//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-            StringBuilder connectStringBuilder = new StringBuilder();
-            connectStringBuilder.append("jdbc:sqlserver://")
-                    .append(SERVER_NAME).append(":").append(PORT).append(";")
-                    .append("databaseName=").append(DATABASE_NAME).append(";")
-                    .append("user=").append(USERNAME).append(";")
-                    .append("password=").append(PASSWORD).append(";");
-            if (USING_SSL) {
-                connectStringBuilder.append("encrypt=true;trustServerCertificate=true;");
-            }
-            CONNECT_STRING = connectStringBuilder.toString();
-            System.out.println("Connect String có dạng: " + CONNECT_STRING);
-        } catch (Exception ex) {
-        }
-    }
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(CONNECT_STRING);
-    }
-
-    public static void main(String[] args) throws SQLException {
-        Connection conn = getConnection();
-        String dbpn = conn.getMetaData().getDatabaseProductName();
-        System.out.println(dbpn);
-        System.out.println(getFACTORY());
-
-    }
 
     private static final SessionFactory FACTORY;
 
@@ -75,7 +21,7 @@ public class hibernateConfig {
         properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
         properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=Nhom2_DuAn1");
         properties.put(Environment.USER, "sa");
-        properties.put(Environment.PASS, "123456");
+        properties.put(Environment.PASS, "123");
         properties.put(Environment.SHOW_SQL, "true");
 
         conf.setProperties(properties);
@@ -90,6 +36,10 @@ public class hibernateConfig {
 
     public static SessionFactory getFACTORY() {
         return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFACTORY());
     }
 
 }
