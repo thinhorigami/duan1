e3e7e23f9bab68d71e642b1f6f95cfad3ddffaea
thinhorigami <nguyen.tien.thinh.thinhorigami@gmail.com>
2023-03-25 09:39:52 +0700

    03/25/2023 09:39:52

--- duan1/src/main/java/com/nhom2/duan1/Application.java
+++ duan1/src/main/java/com/nhom2/duan1/Application.java
@@ -4,6 +4,9 @@
 
 package com.nhom2.duan1;
 import java.sql.SQLException;
+import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
+import view.QuanLyNhanVien;
 
 /*
     @author thinhorigami (thinntph24396)
@@ -23,6 +26,9 @@ import java.sql.SQLException;
 public class Application{
 
     public static void main(String[] args) throws SQLException, Exception {
-
+        var f = new JFrame();
+        f.setLayout(new MigLayout());
+        f.add(new QuanLyNhanVien());
+        f.setVisible(true);
     }
 }
