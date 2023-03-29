d64d13cedca43a204b02d6a9a9de77e03bbdd5b7
DESKTOP-KU4PIO2\Phuong Bi <nguyenphuongsusi1312@gmail.com>
2023-03-29 11:53:44 +0700

    commit

---
+++ duan1/src/main/java/Domainmodel/ChiTiet.java
@@ -0,0 +1,151 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domainmodel;
+
+/**
+ *
+ * @author Phuong Bi
+ */
+public class ChiTiet {
+    private String id;
+    private String idSP;
+    private String idNSX;
+    private String idMauSac;
+    private String dongSP;
+    private String chatLieu;
+    private String size;
+    private String moTa;
+    private String soLuongTon;
+    private String giaNhap;
+    private String giaBan;
+    private String anh;
+    private String trangThai;
+
+    public ChiTiet() {
+    }
+
+    public ChiTiet(String id, String idSP, String idNSX, String idMauSac, String dongSP, String chatLieu, String size, String moTa, String soLuongTon, String giaNhap, String giaBan, String anh, String trangThai) {
+        this.id = id;
+        this.idSP = idSP;
+        this.idNSX = idNSX;
+        this.idMauSac = idMauSac;
+        this.dongSP = dongSP;
+        this.chatLieu = chatLieu;
+        this.size = size;
+        this.moTa = moTa;
+        this.soLuongTon = soLuongTon;
+        this.giaNhap = giaNhap;
+        this.giaBan = giaBan;
+        this.anh = anh;
+        this.trangThai = trangThai;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdSP() {
+        return idSP;
+    }
+
+    public void setIdSP(String idSP) {
+        this.idSP = idSP;
+    }
+
+    public String getIdNSX() {
+        return idNSX;
+    }
+
+    public void setIdNSX(String idNSX) {
+        this.idNSX = idNSX;
+    }
+
+    public String getIdMauSac() {
+        return idMauSac;
+    }
+
+    public void setIdMauSac(String idMauSac) {
+        this.idMauSac = idMauSac;
+    }
+
+    public String getDongSP() {
+        return dongSP;
+    }
+
+    public void setDongSP(String dongSP) {
+        this.dongSP = dongSP;
+    }
+
+    public String getChatLieu() {
+        return chatLieu;
+    }
+
+    public void setChatLieu(String chatLieu) {
+        this.chatLieu = chatLieu;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public String getSoLuongTon() {
+        return soLuongTon;
+    }
+
+    public void setSoLuongTon(String soLuongTon) {
+        this.soLuongTon = soLuongTon;
+    }
+
+    public String getGiaNhap() {
+        return giaNhap;
+    }
+
+    public void setGiaNhap(String giaNhap) {
+        this.giaNhap = giaNhap;
+    }
+
+    public String getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(String giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    public String getAnh() {
+        return anh;
+    }
+
+    public void setAnh(String anh) {
+        this.anh = anh;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+    
+    
+    
+}
