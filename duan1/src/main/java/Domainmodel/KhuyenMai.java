65ea02fbb3b6d2fb7a0d406fa923eeb9f6bbd9bf
tuphph24187 <concuakicuc2k@gmail.com>
2023-03-30 14:06:34 +0700

    no message

--- duan1/src/main/java/Domainmodel/KhuyenMai.java
+++ duan1/src/main/java/Domainmodel/KhuyenMai.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
 public class KhuyenMai {
 
     private String Id;
@@ -27,5 +27,95 @@ public class KhuyenMai {
 
     private int trangThai;
 
+    public KhuyenMai(String Id, String ma, String tenKM, String ngayBatDau, String ngayKetThuc, Integer muc_giam_gia, Boolean donVi, String moTa, int trangThai) {
+        this.Id = Id;
+        this.ma = ma;
+        this.tenKM = tenKM;
+        this.ngayBatDau = ngayBatDau;
+        this.ngayKetThuc = ngayKetThuc;
+        this.muc_giam_gia = muc_giam_gia;
+        this.donVi = donVi;
+        this.moTa = moTa;
+        this.trangThai = trangThai;
+    }
 
+    public KhuyenMai() {
+    }
+
+    
+    
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTenKM() {
+        return tenKM;
+    }
+
+    public void setTenKM(String tenKM) {
+        this.tenKM = tenKM;
+    }
+
+    public String getNgayBatDau() {
+        return ngayBatDau;
+    }
+
+    public void setNgayBatDau(String ngayBatDau) {
+        this.ngayBatDau = ngayBatDau;
+    }
+
+    public String getNgayKetThuc() {
+        return ngayKetThuc;
+    }
+
+    public void setNgayKetThuc(String ngayKetThuc) {
+        this.ngayKetThuc = ngayKetThuc;
+    }
+
+    public Integer getMuc_giam_gia() {
+        return muc_giam_gia;
+    }
+
+    public void setMuc_giam_gia(Integer muc_giam_gia) {
+        this.muc_giam_gia = muc_giam_gia;
+    }
+
+    public Boolean getDonVi() {
+        return donVi;
+    }
+
+    public void setDonVi(Boolean donVi) {
+        this.donVi = donVi;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+
+    
 }
